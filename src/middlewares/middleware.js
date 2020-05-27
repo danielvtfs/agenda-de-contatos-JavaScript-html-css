3f3d71fe22258958ef34fd403ac84e3e569238aa
danielvtfs <danielvtfs@gmail.com>
2020-05-26 22:44:38 -0300

    first commit

---
+++ src/middlewares/middleware.js
@@ -0,0 +1,31 @@
+exports.middleWareGlobal = (req, res, next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
+    next();
+}
+
+exports.outroMiddleWare = (req, res, next) => {
+    next();
+}
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if (err) {
+        return res.render('404')
+    }
+    next();
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};
+
+exports.loginRequired = (req, res, next) => {
+    if (!req.session.user) {
+        req.flash('errors', 'Você precisa fazer login no sistema para acessar o cadastro.');
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+    next();
+}
